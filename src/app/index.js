b838ce4333c700b78560051752db4ef5364d8c6d
dharmendra <Dharmendrap7@gmail.com>
2019-09-23 07:01:52 -0700

    Applying Middleware: Applying middleware, own logger, redux-logger

--- src/app/index.js
+++ src/app/index.js
@@ -30,7 +30,8 @@
 
 // render(<App />, window.document.getElementById('app'));
 
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import {createLogger} from "redux-logger";
 
 // const initialState={
 //     result : 1,
@@ -83,10 +84,21 @@ switch(action.type){
 return state;
 };
 
+const myLogger = (store)=>(next)=>(action)=>{
+    console.log("myLogger: ", action);
+    next(action);
+}
+//Here this is custome middleware which can be later on as other predefined middleware we will be using
+// It will be called before the store has changed. So as true middleware will be called before store updation.
 
 
-
-const store = createStore(combineReducers({mathReducer, userReducer})); //this is being used as key value pair as mathReducer: mathReducer, but because of ES6 syntak it can be shotened as mathReducer. Here two different states are being used as by mathReducer uses math State and userReducer uses user State
+const store = createStore(
+    combineReducers({mathReducer, userReducer}),
+    {},
+    applyMiddleware(myLogger, createLogger())
+); //this is being used as key value pair as mathReducer: mathReducer, but because of ES6 syntak it can be shotened as mathReducer. Here two different states are being used as by mathReducer uses math State and userReducer uses user State
+// Here the second parameter for createStore is to pass initial state which can be passed by reducer which has their own initial state.
+//logger is used from redux-logger which logs the actions.
 
 //So we are having one global state which has sub state
 
